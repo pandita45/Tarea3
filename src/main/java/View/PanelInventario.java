a24476c364cdc96cb5fe792bd97d4bca91017418
SERVYREX <codbryxp@gmail.com>
2025-06-06 20:48:34 -0400

    Representacion numerica de stock de inventario

--- src/main/java/View/PanelInventario.java
+++ src/main/java/View/PanelInventario.java
@@ -1,14 +1,22 @@
 package View;
+import Algoritmo.ProductoYPrecios;
+
 import javax.swing.*;
 import java.awt.*;
+
+import static java.awt.Color.red;
+
 public class PanelInventario extends JPanel {
     PanelCocaCola coca;
     PanelSprite sprite;
     PanelFanta fanta;
     PanelSnickers snickers;
     PanelSuper8 super8;
+    private Texto invStocks;
     public PanelInventario() {
         super();
+        invStocks = new Texto("null");
+        invStocks.setBounds(1040, 850, 450, 30);
         coca = new PanelCocaCola();
         sprite = new PanelSprite();
         fanta = new PanelFanta();
@@ -16,11 +24,13 @@ public class PanelInventario extends JPanel {
         super8 = new PanelSuper8();
         setSize(1920,1080);
         setOpaque(false);
+        setLayout(null);
         add(coca);
         add(sprite);
         add(fanta);
         add(snickers);
         add(super8);
+        add(invStocks);
 
     }
     protected void paintComponent(Graphics g) {
@@ -33,6 +43,15 @@ public class PanelInventario extends JPanel {
             } catch (Exception e) {
                 System.out.println("Error al cargar la imagen del inventario");
             }
+            invStocks.setText("x"+PanelComprador.comprador.cuantosProductos(ProductoYPrecios.COCACOLA)+
+                    "        x"+PanelComprador.comprador.cuantosProductos(ProductoYPrecios.SPRITE)+
+                    "        x"+PanelComprador.comprador.cuantosProductos(ProductoYPrecios.FANTA)+
+                    "        x"+PanelComprador.comprador.cuantosProductos(ProductoYPrecios.SNICKER)+
+                    "        x"+PanelComprador.comprador.cuantosProductos(ProductoYPrecios.SUPER8));
+            invStocks.setVisible(true);
+        }
+        else {
+            invStocks.setVisible(false);
         }
     }
 }
